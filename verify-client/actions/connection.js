b9f6a1f983bd0e2b37e98dde8db0e68a3f497c76
Mikkel Christensen <christensen.mikkel@gmail.com>
2018-01-25 16:27:14 +0100

    Client now fetches connections with the correct (dashboard admin)
    token.

--- verify-client/actions/connection.js
+++ verify-client/actions/connection.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import * as constants from '../constants';
 
 export function fetchConnections() { // eslint-disable-line import/prefer-default-export
-  return {
-    type: constants.FETCH_CONNECTIONS,
-    payload: {
-      promise: axios.get('/api/connections', {
-        responseType: 'json'
-      })
-    }
-  };
+    return {
+        type: constants.FETCH_CONNECTIONS,
+        payload: {
+            promise: axios.get('/api/connections', {
+                headers : {
+                    'Authorization' : 'Bearer ' + sessionStorage.getItem('delegated-admin:apiToken')
+                },
+                responseType: 'json'
+            })
+        }
+    };
 }
