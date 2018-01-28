16d9553ecb9d44dc5b31dea1b840a15d4f09bc17
Mikkel Christensen <christensen.mikkel@gmail.com>
2018-01-28 13:36:04 +0100

    Added a console.log statement in the meta handler, to see if this endpoint
    is called by the infrastructure.

--- server/routes/meta.js
+++ server/routes/meta.js
@@ -4,6 +4,7 @@ import metadata from '../../webtask.json';
 export default () => {
   const api = express.Router();
   api.get('/', (req, res) => {
+    console.log('meta endpoint requested');
     res.status(200).send(metadata);
   });
 
